bb07255d008de866b6d2a2881e1a0c2202fec162
Steve Jamesson <steve@groovywebdesign.com>
2014-01-21 10:09:34 -0500

    gruntfile rsync ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,41 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-
+    app: {
+      src: './public',
+      dest: './dist',
+      secrets: '<%= grunt.file.readJSON("secrets.json") %>'
+    },
+    harp: {
+      server: {
+        server: true,
+        source: '<%= app.src %>'
+      },
+      dist: {
+        source: '<%= app.src %>',
+        dest: '<%= app.dest %>'
+      }
+    },
+    rsync: {
+      options: {
+        args: ['--verbose'],
+        exclude: ['.git*', '.less', 'node_modules'],
+        recursive: true
+      },
+      stage: {
+        options: {
+          src: '<%= app.dest %>',
+          dest: '<%= app.secrets.rsync.stage.dest %>',
+          host: '<%= app.secrets.rsync.stage.host %>'
+        }
+      }
+    }
   });
 
   grunt.registerTask('default', []);
+
+  grunt.registerTask('build', [
+    'harp:dist'
+  ]);
 
 };
